9c366fcf11b209694f459ca5f8ce545844bd9df8
Kevin Kelly <kvinklly@gmail.com>
2018-01-04 14:20:30 -0600

    added more params examples

--- app.js
+++ app.js
@@ -6,6 +6,30 @@ app.get('/', function(req, res) {
   res.send('Hello world!');
 });
 
+app.get('/speak/:animal', function(req, res) {
+  var animal = req.params.animal.toLowerCase();
+  var sounds = {
+    pig: 'Oink',
+    cow: 'Moo',
+    dog: 'Wolf', 
+    cat: 'Meow'
+  }
+  if (sounds[animal] === undefined) {
+    res.status(404);
+    res.send('Animal not found.')
+  } else {
+    res.send(`The ${animal} says '${sounds[animal]}'`);
+  }
+});
+
+app.get('/repeat/:word/:times', function(req, res) {
+  var response = '';
+  for (var count = 0; count < req.params.times; count++) {
+    response += req.params.word + " ";
+  }
+  res.send(response);
+});
+
 app.get('/page/:myPage', function(req, res) {
   res.send('Welcome to page: ' + req.params.myPage);
 })
